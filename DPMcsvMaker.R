4f198659c2522563954efa8a792e897d51f53bbe
ParkKyuSeon <oe135@naver.com>
2020-09-18 02:19:07 +0900

    DPM csv file offer

--- DPMcsvMaker.R
+++ DPMcsvMaker.R
@@ -9,11 +9,13 @@ DPM12338Ratio <- round(DPM12338Ratio, 4)
 rownames(DPM12338Ratio) <- c("DPMRatio", "LeakageRatio", "RestraintRatio", "40sRatio")
 
 DPM12338Final <- rbind(DPM12338, DPM12338Ratio)
+rownames(DPM12338Final) <- c("DPM", "DPMRatio", "DamageLeakage", "LeakageRatio", "Restraint", "RestraintRatio", "40s", "40sRatio")
 for(i in 1:nrow(DPM12338)) {
   DPM12338Final[i*2 - 1, ] <- DPM12338[i, ]
   DPM12338Final[i*2, ] <- DPM12338Ratio[i, ]
 }
-DPM12338Final <- t(DPM12338Final)
+DPM12338Final <- data.frame(t(DPM12338Final))
+DPM12338Final <- DPM12338Final[order(DPM12338Final$DPMRatio, decreasing=T), ]
 write.csv(DPM12338Final, "DPM_12338.csv")
 
 ## Need to be made to Function
